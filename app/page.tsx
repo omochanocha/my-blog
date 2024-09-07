999f25ed4600c9afbc90e09785261453d9eb4bfa
omochanocha <yokpef@gmail.com>
2024-09-08 01:33:41 +0900

    環境構築

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Image from 'next/image';
 
-// type Sample = {
-//   title: string;
-// };
-
 export default function Home(): JSX.Element {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
