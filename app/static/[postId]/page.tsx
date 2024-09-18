efdb07bf68d4c201a2d1b34a4479cdbdb86dfc61
omochanocha <yokpef@gmail.com>
2024-09-19 06:52:53 +0900

    チュートリアル static renderingまで

---
+++ app/static/[postId]/page.tsx
@@ -0,0 +1,48 @@
+import parse from 'html-react-parser';
+import { notFound } from 'next/navigation';
+
+import { getDetail, getList } from '../../../libs/microcms';
+
+/**
+ * この関数があるとstatic renderingになる
+ * @returns
+ */
+export async function generateStaticParams(): Promise<
+  {
+    postId: string;
+  }[]
+> {
+  const { contents } = await getList();
+
+  const paths = contents.map((post) => {
+    return {
+      postId: post.id,
+    };
+  });
+
+  return [...paths];
+}
+
+export default async function StaticDetailPage({
+  params: { postId },
+}: {
+  params: { postId: string };
+}): Promise<JSX.Element> {
+  const post = await getDetail(postId);
+
+  // ページの生成された時間を取得
+  const time = new Date().toLocaleString();
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <div>
+      <p>{postId}</p>
+      <h1>{post.title}</h1>
+      <h2>{time}</h2>
+      <div>{parse(post.content)}</div>
+    </div>
+  );
+}
