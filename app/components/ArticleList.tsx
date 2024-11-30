8bad7e29da362884fd5e15da3d4ab58439ab9aec
omochanocha <yokpef@gmail.com>
2024-12-01 02:37:35 +0900

    記事が複数行になったときようにcssを調整

--- app/components/ArticleList.tsx
+++ app/components/ArticleList.tsx
@@ -12,20 +12,17 @@ export const ArticleList = async (): Promise<JSX.Element> => {
 
   return (
     <div className="mt-8 grid justify-items-center">
-      <ul className="mx-auto flex max-w-4xl gap-4">
+      <ul className="mx-auto grid max-w-5xl grid-cols-3 gap-4">
         {contents.map((post) => {
           return (
-            <li
-              key={post.id}
-              className="grid w-96 overflow-hidden rounded-lg bg-white drop-shadow-lg"
-            >
+            <li key={post.id} className="grid overflow-hidden rounded-lg bg-white drop-shadow-lg">
               <Link href={`/${post.id}`} className="">
                 <div>
                   <Image
-                    src={post.eyecatch?.url ?? 'https://placehold.jp/1200x630.png'}
+                    src={post.eyecatch?.url ?? 'https://placehold.jp/660x346.png'}
                     alt="アイキャッチ画像"
-                    width={post.eyecatch?.width ?? 1200}
-                    height={post.eyecatch?.height ?? 630}
+                    width={post.eyecatch?.width ?? 330}
+                    height={post.eyecatch?.height ?? 173}
                   />
                 </div>
                 <p className="text-slate-800">{post.title}</p>
