2703ce8d199b23a1a3ab5cba78cced2dfdad5f04
omochanocha <yokpef@gmail.com>
2024-12-01 02:37:35 +0900

    css調整

---
+++ app/[postId]/page.tsx
@@ -0,0 +1,69 @@
+import parse from 'html-react-parser';
+import { notFound } from 'next/navigation';
+
+import { getDetail, getList } from '../../libs/microcms';
+import styles from '../page.module.css';
+
+/**
+ * この関数があるとstatic renderingになる
+ * @returns は次のようになる
+ * const paths = [
+ *  {postId: "1"},
+ *  {postId: "2"},
+ *  {postId: "abc3"}
+ * ]
+ */
+export async function generateStaticParams(): Promise<
+  {
+    postId: string;
+  }[]
+> {
+  // generateStaticParams関数内でfetchリクエストでコンテンツを取得した場合はそのリクエストは自動的にメモ化される
+  // 今回は違う
+  const { contents } = await getList();
+
+  const paths = contents.map((post) => {
+    return {
+      postId: post.id,
+    };
+  });
+
+  return [...paths];
+}
+
+/**
+ *
+ * @param paramsにはgenerateStaticParams関数が返すpaths配列の中のこのページのpostIdが入る
+ * @returns JSXElement
+ */
+export default async function StaticDetailPage({
+  // ここをparamsだけにするとpostIdは44行目のように取得することになる
+  params: { postId },
+}: {
+  params: { postId: string };
+}): Promise<JSX.Element> {
+  // const { postId } = params;
+  const post = await getDetail(postId);
+
+  // ページの生成された時間を取得
+  const time = new Date().toLocaleString();
+
+  if (post == null) {
+    notFound();
+  }
+
+  if (post?.category == null) {
+    return <p></p>;
+  }
+
+  return (
+    <div className="text-slate-800">
+      <h1>記事詳細ページ</h1>
+      <p>{postId}</p>
+      <h2>{post?.title}</h2>
+      <h2>{time}</h2>
+      <p>{post?.category.name}</p>
+      <div className={styles['prose']}>{parse(post?.content)}</div>
+    </div>
+  );
+}
---
+++ app/components/ArticleList.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { getList } from '../../libs/microcms';
+
+export default async function ArticleList(): Promise<JSX.Element> {
+  const { contents } = await getList();
+
+  if (contents == null || contents.length === 0) {
+    return <h1>No contents</h1>;
+  }
+
+  return (
+    <div className="mt-8 grid justify-items-center">
+      <ul className="mx-auto max-w-4xl">
+        {contents.map((post) => {
+          return (
+            <li key={post.id} className="grid w-96 overflow-hidden rounded-lg bg-white drop-shadow">
+              <Link href={`/${post.id}`} className="">
+                <Image
+                  src={post.eyecatch?.url ?? ''}
+                  alt="aaa"
+                  width={post.eyecatch?.width}
+                  height={post.eyecatch?.height}
+                />
+                <p className="text-slate-800">{post.title}</p>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
