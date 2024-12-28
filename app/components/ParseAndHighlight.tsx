9c279334a6da68400f189756e4e0a72f43cc9abf
omochanocha <yokpef@gmail.com>
2024-12-29 04:43:56 +0900

    コード整理

--- app/components/ParseAndHighlight.tsx
+++ app/components/ParseAndHighlight.tsx
@@ -45,20 +45,22 @@ export const ParseAndHighlight = (rawHtml: string): string | JSX.Element | JSX.E
 
       // コードブロックの処理
       const dataFileName = domNode.attribs['data-filename'];
-
       if (!isElement(domNode.firstChild)) return;
       const codeElement = dataFileName != null ? domNode.firstChild.firstChild : domNode.firstChild;
-
       if (!isElement(codeElement)) return;
       if (!isText(codeElement.firstChild)) return;
 
-      const code = codeElement.firstChild.data;
+      if (codeElement.attribs['class'] != null) {
+        const code = codeElement.firstChild.data;
 
-      if (codeElement.attribs['class'] == null) return;
+        if (codeElement.attribs['class'] == null) return;
 
-      const language = codeElement.attribs['class'].replace('language-', '');
+        const language = codeElement.attribs['class'].replace('language-', '');
 
-      return <HighlightCode hlc={{ code, language, dataFileName }} />;
+        return <HighlightCode hlc={{ code, language, dataFileName }} />;
+      }
+
+      return;
     },
   });
 };
