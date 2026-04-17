a165afb4214db08e7ad13dc81cd0e3c2a9ad60ae
omochanocha <76525239+omochanocha@users.noreply.github.com>
2026-04-17 19:04:56 +0900

    ヘッダーのアニメーション追加

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -32,7 +32,7 @@ export const Header: React.FC = () => {
 
   return (
     <header
-      className={`grid h-14 w-full shrink-0 border-b border-b-zinc-600 lg:h-16 ${isSticky ? 'sticky top-0 z-10 backdrop-blur-[6px]' : 'relative'}`}
+      className={`sticky grid h-14 w-full shrink-0 border-b border-b-zinc-600 transition-all duration-300 ease-in-out lg:h-16 ${isSticky ? 'top-0 z-10 backdrop-blur-[6px]' : '-top-full'}`}
     >
       <div className="relative mx-auto flex w-full max-w-5xl items-center justify-between gap-x-2 px-4 md:pl-4 md:pr-16">
         <div className="left-10 grid grow md:grow-0">
